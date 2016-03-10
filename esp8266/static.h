5474cdf711f8834466c67be0a13e9a4a73b1ec1c
FloatHub DevOne <thor@floathub.com>
2016-03-10 10:18:12 -0500

    Working to the point of displaying logo, simple-css login screen.

---
+++ esp8266/static.h
@@ -0,0 +1,11 @@
+/*
+
+ FloatHub Graphics
+ (c) 2015-2016 Modiot Labs
+ 
+ Note that this file is _not_ licensed MIT like the actual code.
+ This/these image(s) are trademarks of Modiot Labs
+ 
+*/
+
+const char HTTP_200[] PROGMEM = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
