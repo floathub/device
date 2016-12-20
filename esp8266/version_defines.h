7f801eed56e2661555f9f847abcf879e5e204bc2
FloatHub DevOne <thor@floathub.com>
2016-12-20 11:13:38 -0500

    Started release branch for FloatHub WiFi v1.0 (technically "FHW.P9.K6.v1.0")

--- esp8266/version_defines.h
+++ esp8266/version_defines.h
@@ -1,11 +1,11 @@
 /*
 
-   Versiong for protocol, device, etc.
+   Versioning for protocol, device, etc.
  
 */
 
 
 #define  FLOATHUB_PROTOCOL_VERSION 1
 #define  FLOATHUB_ENCRYPT_VERSION  2
-#define  FLOATHUB_MODEL_DESCRIPTION "FHW.P9.K6"
+#define  FLOATHUB_MODEL_DESCRIPTION "FHW.P9.K6.v1.0"
 
